b7916f9f5c79bad1c039244e2d5eca69d18c5b69
prem <pphansuriyanon@gmail.com>
2023-04-26 03:03:57 +0700

    feat: architecture dir arg for sign.sh

--- sign.sh
+++ sign.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
 
+# sign.sh - signs pacman packages (zst files) in ARCH_DIR (defaults to x86_64)
 # TODO: Use passphrase-fd flag
-PASSPHRASE=$1;
 
-for f in *.zst; do
+[ -z $1 ]\
+  && printf "sign.sh: passphrase required\nUsage: sign.sh [ARCH_DIR] <PASSPHRASE>"\
+  && exit 1;
+
+if [ -z $2 ]; then
+  ARCH_DIR="x86_64";
+  PASSPHRASE=$1;
+else
+  ARCH_DIR=$1;
+  PASSPHRASE=$2;
+fi;
+
+for f in "$ARCH_DIR"/*.zst; do
   gpg --detach-sign --pinentry-mode loopback --passphrase $PASSPHRASE --output $f.sig --sign $f
   #gpg --detach-sign --pinentry-mode loopback --passphrase --passphrase-fd 0 --output $f.sig --sign $f 
 done;
